95d664c0cef2b28cce99b37af41c9e20e15fbcd3
DYfly <1971302380@qq.com>
2019-07-12 16:36:29 +0800

    抓取秀动网数据

--- xiudong/pipelines.py
+++ xiudong/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from xiudong.datacollector.mysqlhelper import MysqlHelper
+from xiudong.dbhelper.mysqlhelper import MysqlHelper
 import json
 
 
